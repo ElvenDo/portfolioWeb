7012e4905ee31bb831ad7752da9c104cf7333b6d
ElvenDo <e_doher@stu-digital-campus.fr>
2025-06-12 18:49:34 +0200

    correct bug

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './scss/index.scss'
 import NotFound from './page/NotFound.jsx'
 import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
+import PageProjet from './page/projet.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/projets/:slug' element={<PageProjet />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </Router>
+
   </StrictMode>,
 )
