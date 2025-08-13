8e732c2971896e448177b3bf7f9f1d03d8cebac6
ElvenDo <e_doher@stu-digital-campus.fr>
2025-08-13 21:19:45 +0200

    bug display

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,45 @@
 import '../scss/header.scss'
 import LogoED from '../assets/LogoED.svg'
+import { useEffect } from 'react'
 
 export default function Header() {
+
+    useEffect(() => {
+        const links = document.querySelectorAll('a[href^="#"]')
+        links.forEach(link => {
+            link.addEventListener('click', e => {
+                e.preventDefault()
+                const target = document.querySelector(link.getAttribute('href'))
+                if (target) {
+                    target.scrollIntoView({ behavior: 'smooth' })
+                }
+            })
+        })
+    }, [])
+
     return (
         <header>
             <img src={LogoED} alt="Logo ElvenDoher" />
             <nav>
                 <ul>
-                    <li><a href="#competences">Compétences</a><div id="hover"></div></li>
-                    <li><a href="#apropos">A propos</a><div id="hover"></div></li>
-                    <li><a href="#projets">Projets</a><div id="hover"></div></li>
-                    <li><a href="#contact">Contat</a><div id="hover"></div></li>
+                    <li>
+                        <a href="#competences">Compétences</a>
+                        <div id="hover"></div>
+                    </li>
+                    <li>
+                        <a href="#apropos">A propos</a>
+                        <div id="hover"></div>
+                    </li>
+                    <li>
+                        <a href="#projets">Projets</a>
+                        <div id="hover"></div>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                        <div id="hover"></div>
+                    </li>
                 </ul>
             </nav>
         </header>
     )
-
 }
